c11b3e02b3deb76a1e06220d7dadc109c4946eaf
feroult <feroult@gmail.com>
2015-09-21 15:49:16 -0300

    yawptut 7

--- src/main/java/yawptut/shields/PersonShield.java
+++ src/main/java/yawptut/shields/PersonShield.java
@@ -1,14 +1,12 @@
 package yawptut.shields;
 
-import io.yawp.repository.IdRef;
 import io.yawp.repository.shields.Shield;
 import yawptut.models.Person;
 
 public class PersonShield extends Shield<Person> {
 
-    @Override
-    public void show(IdRef<Person> id) {
-        allow();
-    }
-
+	@Override
+	public void defaults() {
+		allow();
+	}
 }
